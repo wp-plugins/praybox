b9a1ed440f8842692b593b3ff6f7a057ff156873
guilddev <guilddev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-08 17:40:57 +0000

    git-svn-id: https://plugins.svn.wordpress.org/praybox/tags/0.4@406850 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/inc_pb_crons.php
+++ inc/inc_pb_crons.php
@@ -1,15 +1,7 @@
 <?php
-add_action('prayer_gap','do_gap_check');
-add_action('daily_emails','send_daily_emails');
-
-function setup_pb_crons() {
+function gd_pb_crons(){
 	wp_schedule_event(time(),'hourly','prayer_gap');
 	wp_schedule_event(time(),'daily','daily_emails');
-}
-
-function deactivate_pb_crons() {
-	wp_clear_scheduled_hook('prayer_gap');
-	wp_clear_scheduled_hook('daily_emails');
 }
 
 function do_gap_check() {
@@ -51,16 +43,17 @@ function do_gap_check() {
 }
 
 function send_daily_emails() {
+	// do something every day
 	global $wpdb;
 		
 	$onedayago=strtotime("-1 day");
 	
-	$daily_prayers=$wpdb->get_results("SELECT request_id FROM ".$wpdb->prefix."pb_prayedfor WHERE prayedfor_date>'$onedayago' GROUP BY request_id");
+	$daily_prayers=$wpdb->get_results("SELECT request_id FROM ".$wpdb->prefix."pb_prayedfor WHERE date>'$onedayago' GROUP BY request_id");
 	
 	foreach($daily_prayers as $prayer){
 		$request_id=$prayer->request_id;
 		
-		$num_prayers=$wpdb->get_var($wpdb->prepare("SELECT COUNT(id) FROM ".$wpdb->prefix."pb_prayedfor WHERE prayedfor_date>'$onedayago' AND request_id='$request_id'"));
+		$num_prayers=$wpdb->get_var($wpdb->prepare("SELECT COUNT(id) FROM ".$wpdb->prefix."pb_prayedfor WHERE prayedfor_date>'$onedayago' AND request_id='$request_id';"));
 	
 		$prayer_request=$wpdb->get_row("SELECT first_name,last_name,email,title,notify,authcode FROM ".$wpdb->prefix."pb_requests WHERE id='$request_id'");
 		
@@ -75,17 +68,16 @@ function send_daily_emails() {
 	
 			$management_url=site_url()."/?page_id=".get_option('pb_management_page')."&pbid=$authcode";
 			
-		   	$site_name=get_bloginfo('name');
 		   	$email_from=get_option('pb_reply_to_email');
 		   	$email_subject=get_option('pb_email_subject');
 		   	$email_message=get_option('pb_email_prefix');
 		   	$email_message.="\n\nYour prayer request titled \"$title\" has been lifted up $num_prayers times in the past 24 hours. If you would like to edit your prayer request or submit a praise report for an answered prayer, click here: $management_url\n\nYou will receive an email at the end of each day that your prayer request is lifted up to the Lord letting you know how many times you were prayed for that day.\n\n";
 		   	$email_message.=get_option('pb_email_suffix');
-			$headers.= 'From: '.$site_name.' <'.$email_from.'>' . "\r\n";
-			$headers.= 'Reply-To: '.$site_name.' <'.$email_from.'>' . "\r\n";
+			$headers = 'Reply-To: '.$email_from . \\"\r\n\\";
 		   	
 		   	wp_mail($email,$email_subject,$email_message,$headers);
 	
+		sleep(1);	
 		}		
 	}
 }
