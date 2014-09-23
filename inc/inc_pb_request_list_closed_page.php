fb609802d1215e092f264cfb395a2778dced27cc
blazingtorch <blazingtorch@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-23 16:21:00 +0000

    PrayBox 1.2 - WP 4.0
    git-svn-id: https://plugins.svn.wordpress.org/praybox/tags/1.2@995519 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/inc_pb_request_list_closed_page.php
@@ -0,0 +1,47 @@
+<?php
+
+function pb_request_list_closed_page() {
+global $wpdb;
+?>
+
+<div class="wrap">
+<h2 class="logo-title">PrayBox Closed Prayer Request List</h2>
+<div id="pbx-wrap">
+<?php
+if($_POST['action']=="remove_request"){
+	$req_id=$_POST['pb_request_id'];
+	$wpdb->query("DELETE FROM ".$wpdb->prefix."pb_requests WHERE id='$req_id'");
+	$wpdb->query("DELETE FROM ".$wpdb->prefix."pb_flags WHERE request_id='$req_id'");
+?>
+<p><strong><?php _e('Request Removed.','menu-test'); ?></strong></p>
+<?php } ?>
+
+<?php
+if($_POST['action']=="reopen_request"){
+	$req_id=$_POST['pb_request_id'];
+	$time_now=time();
+	$wpdb->update($wpdb->prefix.'pb_requests',array('closed'=>0,'closed_comment'=>'','active'=>1),array('id'=>$req_id));
+?>
+<p><strong><?php _e('Request Reopened.','menu-test'); ?></strong></p>
+<?php } ?>
+
+<?php
+if($_POST['action']=="archive_request"){
+	$req_id=$_POST['pb_request_id'];
+	$wpdb->update($wpdb->prefix.'pb_requests',array('active'=>3),array('id'=>$req_id));
+?>
+<p><strong><?php _e('Request Archived.','menu-test'); ?></strong></p>
+<?php } ?>
+
+<h3>Closed Prayer Requests</h3>
+
+<table class="pbx-data">
+<tr><th>ID</th><th>First/Last/Email</th><th width="250">Prayer Request</th><th>IP</th><th>Posted</th><th># Prayers</th><th>&nbsp;</th></tr>
+
+<?php
+echo getRequestList('closed');
+?>
+</table>
+</div>
+</div>
+<?php }
---
+++ inc/inc_pb_request_list_flagged_page.php
@@ -0,0 +1,74 @@
+<?php
+function pb_request_list_flagged_page() {
+global $wpdb;
+?>
+
+<div class="wrap">
+<h2 class="logo-title">PrayBox Flagged Prayer Requests</h2>
+<div id="pbx-wrap">
+
+<?php
+if($_POST['action']=="remove_request"){
+	$req_id=$_POST['pb_request_id'];
+	$wpdb->query("DELETE FROM ".$wpdb->prefix."pb_requests WHERE id='$req_id'");
+	$wpdb->query("DELETE FROM ".$wpdb->prefix."pb_flags WHERE request_id='$req_id'");
+?>
+<p><strong><?php _e('Request Removed.','menu-test'); ?></strong></p>
+<?php } ?>
+
+<?php
+if($_POST['action']=="clear_flags"){
+	$req_id=$_POST['pb_request_id'];
+	$wpdb->query("DELETE FROM ".$wpdb->prefix."pb_flags WHERE request_id='$req_id'");
+?>
+<p><strong><?php _e('Flags Cleared.','menu-test'); ?></strong></p>
+<?php } ?>
+
+<?php
+if($_POST['action']=="remove_ban"){
+	$req_id=$_POST['pb_request_id'];
+	$ip=$_POST['pb_ip_address'];
+	$time_now=time();
+	$wpdb->query("DELETE FROM ".$wpdb->prefix."pb_requests WHERE id='$req_id'");
+	$wpdb->query("DELETE FROM ".$wpdb->prefix."pb_flags WHERE request_id='$req_id'");
+	$wpdb->insert($wpdb->prefix.'pb_banned_ips',array('ip_address'=>$ip,'banned_date'=>$time_now,'reason'=>'request flagged as inappropriate'))
+?>
+<p><strong><?php _e('Request Removed and IP Address Banned.','menu-test'); ?></strong></p>
+<?php } ?>
+
+<table class="pbx-data">
+<tr><th>ID</th><th>First/Last/Email</th><th>Title</th><th width="300">Body</th><th>IP Address</th><th>Date Posted</th><th># Times Flagged</th><th>&nbsp;</th></tr>
+
+<?php
+$flags=$wpdb->get_results("SELECT request_id FROM ".$wpdb->prefix."pb_flags GROUP BY request_id");
+
+if($flags){
+	foreach($flags as $flag){
+		$req_id=$flag->request_id;
+		$num_flags=howManyFlags($req_id);
+		
+		$request=$wpdb->get_row("SELECT first_name,last_name,email,title,body,ip_address,submitted FROM ".$wpdb->prefix."pb_requests WHERE id='$req_id'");
+		
+		$first_name=$request->first_name;
+		$last_name=$request->last_name;
+		$email=$request->email;
+		$title=stripslashes($request->title);
+		$body=prePgphOutput($request->body);
+		$ip=$request->ip_address;
+		$submitted=date("m-d-y",$request->submitted);
+		
+		echo "<tr><td>$req_id</td><td>$first_name $last_name<br />$email</td><td>$title</td><td>$body</td><td>$ip</td><td>$submitted</td><td>$num_flags</td><td align='center'>";
+		echo "<form method='post'><input type='hidden' name='action' value='remove_request' /><input type='hidden' name='pb_request_id' value='$req_id' /><input type='submit' class='button-secondary' value='Remove' /></form>";
+		echo "<form method='post'><input type='hidden' name='action' value='clear_flags' /><input type='hidden' name='pb_request_id' value='$req_id' /><input type='submit' class='button-secondary' value='Clear Flags' /></form>";
+		echo "<form method='post'><input type='hidden' name='action' value='remove_ban' /><input type='hidden' name='pb_request_id' value='$req_id' /><input type='hidden' name='pb_ip_address' value='$ip' /><input type='submit' class='button-secondary' value='Remove/Ban' /></form>";
+		echo "</td></tr>";
+	}
+}else{
+	echo "<tr><td colspan='8'>There are currently no flagged prayer requests.</td></tr>";
+}
+
+?>
+</table>
+</div>
+</div>
+<?php }
